a2334c957231a3ef8d1701625bfcd3d5f5bc256e
Elaine Anderson <eparie@bu.edu>
2018-05-24 14:16:27 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,18 +11,27 @@ end
 
 def sort_array_char_count(array)
   array.sort do |a, b|
-  if a.length == b.length
-    0
-  elsif a.length < b.length
-    -1
-  elsif a.length > b.length
-    1
-    end
+  #if a.length == b.length
+  #  0
+  #elsif a.length < b.length
+  #  -1
+  #elsif a.length > b.length
+  #  1
+  #  end
+  a.length <=> b.length
   end
 end
 
 
-def swap_elements
+def swap_elements(array)
+  array.sort do |a, b|
+  if a == b
+    0
+  elsif a < b
+    -1
+  elsif a > b
+    1
+  end
 end
 
 def reverse_array
