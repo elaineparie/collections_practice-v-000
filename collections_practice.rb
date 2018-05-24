169f1e318bff3151c8bc220c05113e404517341e
Elaine Anderson <eparie@bu.edu>
2018-05-24 14:25:31 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,7 +20,9 @@ def swap_elements(array)
   array [0], array[1], array[2] = array [0], array[2], array[1]
 end
 
-def reverse_array
+def reverse_array(integers)
+  integers.sort do |a, b|
+    b <=> a
 end
 
 def kesha_maker
